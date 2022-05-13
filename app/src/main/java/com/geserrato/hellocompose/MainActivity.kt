05beb01f8e73e09f52a09b35173cd7cab580f5a6
Gerardo Estrella <gerardo.estrella@sygno.com.mx>
2022-05-12 22:11:59 -0500

    Initial commit

---
+++ app/src/main/java/com/geserrato/hellocompose/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.geserrato.hellocompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.geserrato.hellocompose.ui.theme.HelloComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloCompose()
+        }
+    }
+}
+
+@Composable
+fun HelloCompose(){
+    Text("Hello Compose! :(")
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun HelloComposePreview(){
+    HelloCompose()
+}
